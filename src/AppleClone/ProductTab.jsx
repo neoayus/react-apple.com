04e3b86e64c1b1067f602e4ec31f529084743488
neoayus <ayus43@outlook.com>
2026-02-01 00:16:32 +0530

    feat: add various type of cards and group them in Product.jsx

---
+++ src/AppleClone/ProductTab.jsx
@@ -0,0 +1,95 @@
+import { Card, CardBlack, CardReverse} from './Cards'
+
+export default function ProductTab(){
+  return(
+    <div style={{
+      paddingBottom: '12px', 
+      flexDirection: 'column',
+      display: 'flex',
+      gap: '12px'   
+    }}>
+
+      <Card
+        image="../../public/post-one.jpg"
+        title="iPhone"
+        desc="Meet the iPhone 16 family."
+        primaryButtonText="learn more"
+        secondaryButtonText="Shop iPhone"
+        impInfo="Built for Apple Intelligence"
+      />
+
+      <CardBlack
+        image="../../public/ipad-pro.jpg"
+        title="iPad Pro"
+        desc="Unbelievably thin. Incredibly Powerful"
+        primaryButtonText="learn more"
+        secondaryButtonText="Buy"
+        impInfo="Built for Apple Intelligence"
+      />
+      
+      
+
+      <Card
+        image="../../public/macbook-air.jpg"
+        title="MacBook Air"
+        desc={
+          <>
+            sky blue color. <br /> Sky high performance with M4.{" "}
+          </>
+        }
+        primaryButtonText="learn more"
+        secondaryButtonText="Buy"
+      />
+
+      <Card
+        image="../../public/ipad-air.jpeg"
+        title="iPad Air"
+        desc="Now supercharged with M3 Chip."
+        primaryButtonText="learn more"
+        secondaryButtonText="Buy"
+      />
+
+      <Card
+        image="../../public/watch.jpeg"
+        title="WATCH"
+        desc="Thinstant Classic"
+        primaryButtonText="learn more"
+        secondaryButtonText="Buy"
+      />
+      
+      <CardReverse
+        image="../../public/air-pods2.jpg"
+        title="AirPods2"
+        desc="Now with a Heaing Air feature."
+        primaryButtonText="learn more"
+        secondaryButtonText="Buy"
+        // impInfo="Built for Apple Intelligence"
+      />
+
+      <Card
+        image="../../public/trade-in.jpg"
+        title="Trade In"
+        desc={
+          <>
+            Get $170 - $ 630 in credit when you 
+            <br/> trade in iPhone 12 or higher.
+          </>
+        }
+        primaryButtonText="Get your estimate"
+      />
+
+      <Card
+        image="../../public/card.jpeg"
+        title="Card"
+        desc={
+          <>
+            Get up to 3% Daily Cash back
+            <br/> with every purchase
+          </>
+        }
+        primaryButtonText="learn more"
+        secondaryButtonText="Apply Now"
+      />
+    </div>
+  )
+}
